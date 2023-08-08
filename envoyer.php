7cc0490a5afde4e0c8bb5672cd81f9b17311d86c
Tsirym2 <138801257+Tsirym2@users.noreply.github.com>
2023-08-08 10:24:06 +0300

    Update envoyer.php

--- envoyer.php
+++ envoyer.php
@@ -3,28 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RESULTAT CEPE</title>
+    <title>VOTRE RESULTAT </title>
 </head>
 <body>
 <H1>VOTRE RESULTAT</H1>
-<table>
-    <thead>
-        <tr>
-            <th>MATRICULE</th>
-            <th>NOM ET PRENOM</th>
-            <th>OBSERVATION</th>
-        </tr>
-    </thead>
-</table>
 <?php
-include"connexion.php";
-$req=mysqli_query($link,SELECT*FROM "resultat");
-while($res=mysqli_fetch_array($req))
+$user="root";
+$mdp="";
+$bdd="espace-candidat";
+$server="localhost";
+$connect=mysqli_connect($server,$user,$mdp,$bdd);
+if ($connect)
 {
-?>
-<tr>
-<td> <?php echo $res["N°MATRICULE"];?></td>
-<td> <?php echo $res["NOM ET PRENOM"];?></td>
-<td> <?php echo $res["OBSERVATION"];?></td>
-/tr>
+	//echo"connexion etablie";
+}else
+{
+	die(mysqli_connect_error());
 }
+$recherche1="SELECT * FROM `candidat_cepe` WHERE `N°MATRICULE`like'%".$_POST["MATRICULE"]."%'";
+$recherche2="SELECT * FROM `candidat_cepe` WHERE `NOM_ET_PRENOM`like'%".$_POST["NOM_ET_PRENOM"]."%'";
+$resultat=mysql_query($recherche1,$recherche2);
+if($resultat)
+{
+	$candidat=mysqli_num_rows($resultat);
+if($candidat>0)
+{
+	echo"<table border='1'><br>";
+	echo"<tr><br>";
+	echo"<td><strong>MATRICULE</strong></td><br>";
+    echo"<td><strong>NOM ET PRENOM</strong></td><br>";
+	echo"<td><strong>OBSERVATION</strong></td><br>";
+	echo"</tr><br>";
+	while($candidat=mysql_fetch_assoc($resultat)){
+    echo"<tr><br>";
+	echo"<td>".$candidat["MATRICULE"]."</td><br>";
+    echo"<td>".$candidat["NOM_ET_PRENOM"]."</td><br>";
+	echo"<td>".$candidat["OBSERVATION"]."</td><br>";
+    echo"</tr><br>"	;
+	}
+	echo"</table><br>";
+	
+}
+else
+{echo"<p>Désolé ce candidat n'existe pas.</p>";
+}
+}
+else
+{echo"erreur dans l'execurtion de la requête </br>";
+echo"le message d'erreur est:".mysql_error($connect);
+}
+?>
